58750ae532d0957b82ea7fdad56c6c1594d1c647
leander1702 <leanderxavier71@gmail.com>
2025-09-29 10:58:02 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+
+
+
 import Footer from "./components/Footer";
 import "@fontsource/poppins";
 import Home from "./pages/Home/Home";
