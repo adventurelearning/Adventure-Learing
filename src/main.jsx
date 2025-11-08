14d863d12e95e22841492f0e7c6e106213310183
Adventureexamrally <adventure.webapp@gmail.com>
2025-11-08 18:43:19 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "aos/dist/aos.css"; // Import AOS CSS globally
 import AOS from "aos"; // Import AOS JavaScript library
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.jsx'
-import { HelmetProvider } from 'react-helmet-async'
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
-import { HttpLink } from '@apollo/client'
+import { HttpLink } from "@apollo/client";
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
   }),
-})
+});
 
 // Initialize AOS when the app starts
 AOS.init({
@@ -23,21 +23,16 @@ AOS.init({
   once: true, // Animate only once
 });
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
     <BrowserRouter>
       <HelmetProvider>
         <ErrorBoundary>
           <ApolloProvider client={client}>
-            <App />
+            {/* <App /> */}
           </ApolloProvider>
         </ErrorBoundary>
       </HelmetProvider>
     </BrowserRouter>
-
-
-  </StrictMode>,
-)
+  </StrictMode>
+);
